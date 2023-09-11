6056c99f55536d1cb4660a55c10230fae14620de
navas <gabrielnavas@email.com>
2023-09-11 18:44:17 -0300

    remove not use import

--- api/src/test/java/com/checker/api/domain/PlayerTest.java
+++ api/src/test/java/com/checker/api/domain/PlayerTest.java
@@ -1,6 +1,5 @@
 package com.checker.api.domain;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
